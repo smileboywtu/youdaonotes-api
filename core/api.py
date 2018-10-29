7d08994586c3cb154a28106242bc74f1c967030d
smileboywtu <smileboywtu@gmail.com>
2018-10-29 22:17:30 +0800

    update

---
+++ core/api.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+
+class YouDaoNotesClient():
+
+    def __init__(self, customer_key, customer_secret):
+        pass
+
+    def request_auth(self):
+        pass
+
+    def request_token(self):
+        pass
+
